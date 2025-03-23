c9e6226050c98e6344224ba211d4f84ba9271b7c
Nikhil Kale <nikhilrk1444@gmail.com>
2025-03-23 22:24:28 +0530

    initial commit

---
+++ application/src/main/java/com/application/application/Application.java
@@ -0,0 +1,15 @@
+package com.application.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.crypto.spec.PSource;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Nikhil");
+	}
+
+}
